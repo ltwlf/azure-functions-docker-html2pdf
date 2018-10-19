6a153b052013a03b552fc2c7be5c07be6b71e3e3
Christian Glessner <cg@expertsinside.com>
2018-10-19 23:01:00 +0200

    Support for Base64 format

--- Html2Pdf/index.js
+++ Html2Pdf/index.js
@@ -4,42 +4,56 @@ const os = require('os');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const uuidv1 = require('uuid/v1');
+const base64 = require('binary-base64');
 
-module.exports = async function(context, req) {
+module.exports = async function (context, req) {
 
     context.log('HTML2PDF function is processing a request...');
 
-    try{
+    try {
         let tempDir = os.tmpdir();
         let tempFile = `${tempDir}/${uuidv1()}.pdf`;
 
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = await browser.newPage();
 
-        if(req.query.url){
-            await page.goto(req.query.url, {waitUntil: 'networkidle2'});
+        const base64format = req.query.base64 === "true" || false;
+
+        if (req.query.url) {
+            await page.goto(req.query.url, { waitUntil: 'networkidle2' });
         }
-        else{
+        else {
             await page.goto(`data:text/html,${req.body}`, { waitUntil: 'networkidle2' });
         }
-        await page.pdf({path: tempFile, format: 'A4'});
+        await page.pdf({ path: tempFile, format: 'A4' });
         await browser.close();
 
         var data = fs.readFileSync(tempFile);
         fs.unlinkSync(tempFile);
 
-        context.res = {
-            headers: {
-                "Content-Type": "application/pdf"
-            },
-            status: 200,
-            isRaw: true,
-            body: data
-        };
-    }catch(ex){
+        if (base64format) {
+            context.res = {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                status: 200,
+                isRaw: false,
+                body: {
+                    base64: base64.encode(data)
+                }
+            }
+        }
+        else {
+            context.res = {
+                headers: {
+                    "Content-Type": "application/pdf"
+                },
+                status: 200,
+                isRaw: true,
+                body: data
+            }
+        }
+    } catch (ex) {
         context.log.error(ex);
     }
-
-    // not needed in an async function
-    // context.done();
 };
--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 With this Azure Function you can convert an URL or HTML string to a PDF. For the conversion the Function uses internally chromium headless with Puppeteer API.
 
-You can easly host the Function in your own Azure Subscription (Instructions coming soon...) or try it online:
-https://ltwlf-functions-html2pdf.azurewebsites.net 
-
-### Function URL:
-/api/html2pdf
+You can easily host the Function in your own Azure Subscription (Instructions coming soon...) or try it online:
+https://ltwlf-api.azurewebsites.net/public/html2pdf
 
 ### Parameters
 You can either append the query parameter URL to a GET or POST request or you can POST an HTML string in the request body.
@@ -14,5 +11,5 @@ You can either append the query parameter URL to a GET or POST request or you ca
 
 Example GET Request with URL parameter:
 
-https://ltwlf-functions-html2pdf.azurewebsites.net/api/html2pdf?url=http%3A%2F%2Fblog.leitwolf.io
+https://ltwlf-api.azurewebsites.net/public/html2pdf/html2pdf?url=http%3A%2F%2Fblog.leitwolf.io
 
